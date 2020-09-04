2a61cebe235c80c3430c716c00582c41a6d3a5bb
Parima Kashyap <parima001rs@gmail.com>
2020-09-04 15:56:35 +0530

    Account Page

--- main.js
+++ main.js
@@ -27,3 +27,21 @@ SmallImg[2].onclick = function(){
 SmallImg[3].onclick = function(){
     ProductImg.src = SmallImg[4].src;
 }
+
+// --------- JS FOR TOGGLE FORM-----------
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)";
+    Indicator.style.transform = "translateX(100px)";
+}
+
+function login(){
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)";
+    Indicator.style.transform = "translateX(0px)";
+}
